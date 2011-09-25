03aac9bf413230ffd079bcaf65421a7e3832ad38
Alexander Godlewski <alex@implode.ca>
2011-09-25 17:50:39 -0400

    Add command arguments for logout (-l), silent (-s, accepts everything), uninstall (-u)

--- nvid-build.sh
+++ nvid-build.sh
@@ -3,6 +3,26 @@
 KERNELGCCVERSION=$(cat /proc/version | awk '{print $9}' | awk 'BEGIN {FS="."} {print $1"."$2}')
 NVIDIAINSTALLER='./'$(ls -t | grep NVIDIA | head -1)
 
+# Options
+NVBLOGOUT=
+NVBUNINSTALL=
+NVBSILENT=
+
+while getopts lus o
+do
+    case "$o" in
+        l)
+            NVBLOGOUT='y'
+            ;;
+        u)
+            NVBUNINSTALL='--uninstall'
+            ;;
+        s)
+            NVBSILENT='--silent --opengl-headers --run-nvidia-xconfig'
+            ;;
+    esac
+done
+
 if [ ! -e $NVIDIAINSTALLER ]
 then
     echo 'NVIDIA installer not found!'
@@ -15,7 +35,12 @@ then
     exit 1
 fi
 
-/etc/init.d/gdm stop
+sudo /etc/init.d/gdm stop
 echo 'Installing '$NVIDIAINSTALLER
-env CC=/usr/bin/gcc-$KERNELGCCVERSION $NVIDIAINSTALLER
-/etc/init.d/gdm start
+sudo env CC=/usr/bin/gcc-$KERNELGCCVERSION $NVIDIAINSTALLER "$NVBUNINSTALL $NVBSILENT"
+sudo /etc/init.d/gdm start
+
+if [ -n "$NVBLOGOUT" ]
+then
+    kill -HUP $PPID
+fi
